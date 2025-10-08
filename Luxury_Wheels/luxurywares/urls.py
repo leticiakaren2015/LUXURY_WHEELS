d71a2c27101d3e47f2a00ff8e8a1ef7ce0defc7f
leticiakaren2015 <leticiakaren2015@gmail.com>
2025-10-08 22:00:03 +0100

    erro na pagina da reserva

---
+++ Luxury_Wheels/luxurywares/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ======================
+    # Rotas de Cliente
+    # ======================
+    path('clientes/', views.listar_clientes, name='listar_clientes'),
+    path('clientes/criar/', views.criar_cliente, name='criar_cliente'),
+    path('clientes/editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('clientes/excluir/<int:id>/', views.confirmar_excluir_cliente, name='confirmar_excluir_cliente'),
+
+    # ======================
+    # Rotas de Veículo
+    # ======================
+    path('veiculos/', views.listar_veiculos, name='listar_veiculos'),
+    path('veiculos/criar/', views.criar_veiculo, name='criar_veiculo'),
+    path('veiculos/editar/<int:id>/', views.editar_veiculo, name='editar_veiculo'),
+    path('veiculos/excluir/<int:id>/', views.confirmar_excluir_veiculo, name='confirmar_excluir_veiculo'),
+
+    # ======================
+    # Rotas de Reserva
+    # ======================
+    path('reservas/', views.listar_reservas, name='listar_reserva'),
+    path('reservas/criar/', views.criar_reserva, name='criar_reserva'),
+    path('reservas/editar/<int:id>/', views.editar_reserva, name='editar_reserva'),
+    path('reservas/excluir/<int:id>/', views.confirmar_excluir_reserva, name='excluir_reserva'),
+
+    # ======================
+    # Home (página inicial)
+    # ======================
+    path('', views.home, name='home'),
+]
